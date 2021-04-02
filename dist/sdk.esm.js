9a1a956c725735cf48af8fd8de15e5552751a667
zhangzhannan <zhangzhannan@wbf.io>
2021-04-02 17:10:15 +0800

    更改WETH为WHT

--- dist/sdk.esm.js
+++ dist/sdk.esm.js
@@ -445,7 +445,7 @@ var WETH = (_WETH = {},
    _WETH[ChainId.RINKEBY] = /*#__PURE__*/new Token(ChainId.RINKEBY, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether'), 
    _WETH[ChainId.GÖRLI] = /*#__PURE__*/new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'), 
    _WETH[ChainId.KOVAN] = /*#__PURE__*/new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
-   _WETH[ChainId.HECO] = /*#__PURE__*/new Token(ChainId.HECO, '0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f', 18, 'WETH', 'Wrapped Ether'), _WETH);
+   _WETH[ChainId.HECO] = /*#__PURE__*/new Token(ChainId.HECO, '0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f', 18, 'WHT', 'Wrapped HT'), _WETH);
 
 var _toSignificantRoundin, _toFixedRounding;
 var Decimal = /*#__PURE__*/toFormat(_Decimal);
